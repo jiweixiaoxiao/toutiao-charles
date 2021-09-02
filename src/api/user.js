00273fe8f4fdf996e6a31c1846ac6952892e6b0d
charles <jiweixiaoxiao@me.com>
2021-09-02 18:02:01 +0800

    111

--- src/api/user.js
+++ src/api/user.js
@@ -1,15 +1,9 @@
 /**
  * 用户相关请求模块
  */
-
 import request from '@/utils/request'
+// import store from '@/store'
 
-/**
- * 用户登陆
- * 万能验证码
- * 13911111111
- * 验证码： 246810
- */
 export const login = data => {
   return request({
     method: 'POST',
@@ -18,12 +12,19 @@ export const login = data => {
   })
 }
 
-export const getSmsCode = mobile => {
+/**
+ * 发送验证码
+ * 注意：每手机号每分钟1次
+ */
+export const sendSms = mobile => {
   return request({
     method: 'GET',
-    // eslint-disable-next-line no-template-curly-in-string
     url: `/app/v1_0/sms/codes/${mobile}`
-    // http://ttapi.research.itcast.cn/app/v1_0/sms/codes
+  })
+}
+
+export const getSmsCode = mobile => {
+  return request({
   })
 }
 
@@ -50,5 +51,60 @@ export const getUserChannels = () => {
   return request({
     method: 'GET',
     url: '/app/v1_0/user/channels'
+  })
+}
+
+/**
+ * 关注用户
+ */
+export const addFollow = target => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/user/followings',
+    data: {
+      target
+    }
+  })
+}
+
+/**
+ * 取消关注用户
+ */
+export const deleteFollow = target => {
+  return request({
+    method: 'DELETE',
+    url: `/app/v1_0/user/followings/${target}`
+  })
+}
+
+/**
+ * 获取当前登录用户的个人资料
+ */
+export const getUserProfile = target => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user/profile'
+  })
+}
+
+/**
+ * 更新用户资料
+ */
+export const updateUserProfile = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/profile',
+    data
+  })
+}
+
+/**
+ * 更新用户照片资料
+ */
+export const updateUserPhoto = data => {
+  return request({
+    method: 'PATCH',
+    url: '/app/v1_0/user/photo',
+    data
   })
 }
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,11 +1,13 @@
 <template>
-<div class="article-list">
-  <van-pull-refresh
-    v-model="isreFreshLoading"
-    :success-text="refreshSuccessText"
-    :success-duration="1500"
-    @refresh="onRefresh"
-  >
+  <div class="article-list">
+    <!--
+      loading 控制上拉加载更多的 loading 状态
+      finished 控制数据是否加载结束
+      load 事件：当触发上拉加载更多的时候会触发调用 load 事件
+
+      List 初始化后会触发一次 load 事件，用于加载第一屏的数据
+      如果一次请求加载的数据条数较少，导致列表内容无法铺满当前屏幕，List 会继续触发 load 事件，直到内容铺满屏幕或数据全部加载完成
+    -->
     <van-list
       v-model="loading"
       :finished="finished"
@@ -14,36 +16,41 @@
       error-text="请求失败，点击重新加载"
       @load="onLoad"
     >
-<!--      <article-item
-        v-for="(article, index) in list"
-        :key="index"
-        :article="article"
-      />-->
-       <van-cell
+<!--      <van-cell
         v-for="(article, index) in list"
         :key="index"
         :title="article.title"
+      />-->
+      <article-item
+        v-for="(article, index) in list"
+        :key="index"
+        :article="article"
       />
     </van-list>
-  </van-pull-refresh>
-</div>
+  </div>
 </template>
 
 <script>
 import { getArticles } from '@/api/article'
+import ArticleItem from '@/components/article-item'
 export default {
   name: 'ArticleList',
-  components: {},
-  props: {},
+  components: {
+    ArticleItem
+  },
+  props: {
+    channel: {
+      type: Object,
+      required: true
+    }
+  },
   data () {
     return {
-      list: [], // 存储列表数据的数组
-      loading: false, // 控制加载中 loading 状态
-      finished: false, // 控制数据加载结束的状态
-      timestamp: null, // 请求获取下一页数据的时间戳
-      error: false, // 控制列表加载失败的提示状态
-      isreFreshLoading: false, // 控制下拉刷新的 loading 状态
-      refreshSuccessText: '刷新成功' // 下拉刷新成功提示文本
+      list: [], // 文章列表数据
+      loading: false, // 上拉加载更多的 loading 状态
+      finished: false, // 是否加载结束
+      error: false, // 是否加载失败
+      timestamp: null // 请求下一页数据的时间戳
     }
   },
   computed: {},
@@ -51,34 +58,39 @@ export default {
   created () {},
   mounted () {},
   methods: {
+    // 当触发上拉加载更多的时候调用该函数
     async onLoad () {
-      console.log('-------------------')
-      const { data } = await getArticles({
-        channel_id: this.channel.id,
-        timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
-        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
-      })
+      try {
+        // 1. 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id, // 频道 id
+          timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，请求历史推荐传指定的时间戳
+          with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+        })
 
-      const { results } = data.data
-      this.list.push(...results)
-      console.log(this.list)
-      this.loading = false
-      if (results.length) {
-        // 更新获取下一页数据的时间戳
-        this.timestamp = data.data.pre_timestamp
-      } else {
-        // 没有数据了，将 finished 设置为 true，不再 load 加载更多了
-        this.finished = true
+        // 2. 把数据添加到 list 数组中
+        const { results } = data.data
+        this.list.push(...results)
+
+        // 3. 设置本次加载中 loading 状态结束
+        this.loading = false
+
+        // 4. 判断数据是否加载结束
+        if (results.length) {
+          // 更新获取下一页数据的时间戳
+          this.timestamp = data.data.pre_timestamp
+        } else {
+          // 没有数据了，设置加载状态结束，不再触发上拉加载更多了
+          this.finished = true
+        }
+      } catch (err) {
+        console.log(err)
+        this.loading = false // 关闭 loading 效果
+        this.error = true // 开启错误提示
       }
-    },
-
-    onRefresh () {
-      console.log('onRefresh')
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped lang="less"></style>
