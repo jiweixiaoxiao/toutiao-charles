564b6f2e011be47d8c52eb6daa30cedbc4393548
jiwei <jiweixiaoxiao@me.com>
2021-09-03 23:05:13 +0800

    完成了搜索功能的开发

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -15,14 +15,15 @@
         <van-loading
           color="#3296fa"
           vertical
-        >加载中</van-loading>
+        >加载中
+        </van-loading>
       </div>
       <!-- /加载中 -->
 
       <!-- 加载完成-文章详情 -->
       <div v-else-if="article.title" class="article-detail">
         <!-- 文章标题 -->
-        <h1 class="article-title">{{this.article.title}}</h1>
+        <h1 class="article-title">{{ this.article.title }}</h1>
         <!-- /文章标题 -->
 
         <!-- 用户信息 -->
@@ -57,55 +58,98 @@
           ref="article-content"
         ></div>
         <van-divider>正文结束</van-divider>
+        <!-- 文章评论列表 -->
+        <comment-list
+          :source="article.art_id"
+          :list="commentList"
+          @onload-success="totalCommentCount = $event.total_count"
+          @reply-click="onReplyClick"
+        />
+        <!-- /文章评论列表 -->
+        <!-- 底部区域 -->
+        <div class="article-bottom">
+          <van-button
+            class="comment-btn"
+            type="default"
+            round
+            size="small"
+            @click="isPostShow = true"
+          >写评论
+          </van-button>
+          <van-icon
+            class="comment-icon"
+            name="comment-o"
+            :info="totalCommentCount"
+          />
+          <collect-article
+            class="btn-item"
+            v-model="article.is_collected"
+            :article-id="article.art_id"
+          />
+          <like-article
+            class="btn-item"
+            v-model="article.attitude"
+            :article-id="article.art_id"
+          />
+          <van-icon name="share" color="#777777"></van-icon>
+        </div>
+        <!-- /底部区域 -->
+
+        <!-- 发布评论 -->
+        <van-popup
+          v-model="isPostShow"
+          position="bottom"
+        >
+          <comment-post
+            :target="article.art_id"
+            @post-success="onPostSuccess"
+          />
+        </van-popup>
+        <!-- 发布评论 -->
       </div>
       <!-- /加载完成-文章详情 -->
 
       <!-- 加载失败：404 -->
       <div v-else-if="errStatus === 404" class="error-wrap">
-        <van-icon name="failure" />
+        <van-icon name="failure"/>
         <p class="text">该资源不存在或已删除！</p>
       </div>
       <!-- /加载失败：404 -->
 
       <!-- 加载失败：其它未知错误（例如网络原因或服务端异常） -->
       <div v-else class="error-wrap">
-        <van-icon name="failure" />
+        <van-icon name="failure"/>
         <p class="text">内容加载失败！</p>
-        <van-button class="retry-btn">点击重试</van-button>
+        <van-button
+          class="retry-btn"
+          @click="loadArticle"
+        >点击重试
+        </van-button>
       </div>
       <!-- /加载失败：其它未知错误（例如网络原因或服务端异常） -->
     </div>
 
-    <!-- 底部区域 -->
-    <div class="article-bottom">
-      <van-button
-        class="comment-btn"
-        type="default"
-        round
-        size="small"
-      >写评论</van-button>
-      <van-icon
-        name="comment-o"
-        info="123"
-        color="#777"
+    <!-- 评论回复 -->
+    <!--
+      弹出层是懒渲染的：只有在第一次展示的时候才会渲染里面的内容，之后它的关闭和显示都是在切换内容的显示和隐藏
+     -->
+    <van-popup
+      v-model="isReplyShow"
+      position="bottom"
+      style="height: 100%;"
+    >
+      <!--
+        v-if 条件渲染
+          true：渲染元素节点
+          false：不渲染
+       -->
+      <comment-reply
+        v-if="isReplyShow"
+        :comment="currentComment"
+        @close="isReplyShow = false"
       />
-      <collect-article
-        class="btn-item"
-        v-model="article.is_collected"
-        :article-id="article.art_id"
-      />
-      <like-article
-        class="btn-item"
-        v-model="article.attitude"
-        :article-id="article.art_id"
-      />
-      <van-icon
-        color="#777"
-        name="good-job-o"
-      />
-      <van-icon name="share" color="#777777"></van-icon>
-    </div>
-    <!-- /底部区域 -->
+    </van-popup>
+    <!-- /评论回复 -->
   </div>
 </template>
 
@@ -115,16 +159,23 @@ import { ImagePreview } from 'vant'
 import FollowUser from '@/components/follow-user'
 import CollectArticle from '@/components/collect-article'
 import LikeArticle from '@/components/like-article'
+import CommentList from './components/comment-list'
+import CommentPost from './components/comment-post'
+import CommentReply from './components/comment-reply'
+
 export default {
   name: 'ArticleIndex',
   components: {
     FollowUser,
     CollectArticle,
-    LikeArticle
+    LikeArticle,
+    CommentList,
+    CommentPost,
+    CommentReply
   },
   props: {
     articleId: {
-      type: [Number, String],
+      type: [Number, String, Object],
       required: true
     }
   },
@@ -146,23 +197,38 @@ export default {
   created () {
     this.loadArticle()
   },
-  mounted () {},
+  mounted () {
+  },
   methods: {
     async loadArticle () {
       // 展示 loading 加载中
       this.loading = true
       try {
         const { data } = await getArticleById(this.articleId)
+
+        // if (Math.random() > 0.5) {
+        //   JSON.parse('dsankljdnskaljndlkjsa')
+        // }
+
+        // 数据驱动视图这件事儿不是立即的
         this.article = data.data
-        console.log(this.article)
+
+        // 初始化图片点击预览
+        // console.log(this.$refs['article-content'])
         setTimeout(() => {
           this.previewImage()
         }, 0)
+
+        // 请求成功，关闭 loading
+        // this.loading = false
       } catch (err) {
         if (err.response && err.response.status === 404) {
           this.errStatus = 404
         }
+        // this.loading = false
+        // console.log('获取数据失败', err)
       }
+
       // 无论成功还是失败，都需要关闭 loading
       this.loading = false
     },
@@ -186,6 +252,19 @@ export default {
           })
         }
       })
+    },
+    onPostSuccess (data) {
+      // 关闭弹出层
+      this.isPostShow = false
+      // 将发布内容显示到列表顶部
+      this.commentList.unshift(data.new_obj)
+    },
+
+    onReplyClick (comment) {
+      this.currentComment = comment
+
+      // 显示评论回复弹出层
+      this.isReplyShow = true
     }
 
   }
@@ -193,163 +272,185 @@ export default {
 </script>
 
 <style scoped lang="less">
-@import "./github-markdown.css";
-.article-container {
-  .main-wrap {
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 92px;
-    bottom: 88px;
-    overflow-y: scroll;
-    background-color: #fff;
-  }
-  .article-detail {
-    .article-title {
-      font-size: 40px;
-      padding: 50px 32px;
-      margin: 0;
-      color: #3a3a3a;
+  @import "./github-markdown.css";
+
+  .article-container {
+    .main-wrap {
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 92px;
+      bottom: 88px;
+      overflow-y: scroll;
+      background-color: #fff;
     }
 
-    .user-info {
-      padding: 0 32px;
-      .avatar {
-        width: 70px;
-        height: 70px;
-        margin-right: 17px;
-      }
-      .van-cell__label {
-        margin-top: 0;
-      }
-      .user-name {
-        font-size: 24px;
+    .article-detail {
+      .article-title {
+        font-size: 40px;
+        padding: 50px 32px;
+        margin: 0;
         color: #3a3a3a;
       }
-      .publish-date {
-        font-size: 23px;
-        color: #b7b7b7;
+
+      .user-info {
+        padding: 0 32px;
+
+        .avatar {
+          width: 70px;
+          height: 70px;
+          margin-right: 17px;
+        }
+
+        .van-cell__label {
+          margin-top: 0;
+        }
+
+        .user-name {
+          font-size: 24px;
+          color: #3a3a3a;
+        }
+
+        .publish-date {
+          font-size: 23px;
+          color: #b7b7b7;
+        }
+
+        .follow-btn {
+          width: 170px;
+          height: 58px;
+        }
       }
-      .follow-btn {
-        width: 170px;
-        height: 58px;
+
+      .article-content {
+        padding: 55px 32px;
+
+        /deep/ p {
+          text-align: justify;
+        }
       }
     }
 
-    .article-content {
-      padding: 55px 32px;
-      /deep/ p {
-        text-align: justify;
+    .loading-wrap {
+      padding: 200px 32px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #fff;
+    }
+
+    .error-wrap {
+      padding: 200px 32px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background-color: #fff;
+
+      .van-icon {
+        font-size: 122px;
+        color: #b4b4b4;
+      }
+
+      .text {
+        font-size: 30px;
+        color: #666666;
+        margin: 33px 0 46px;
+      }
+
+      .retry-btn {
+        width: 280px;
+        height: 70px;
+        line-height: 70px;
+        border: 1px solid #c3c3c3;
+        font-size: 30px;
+        color: #666666;
+      }
+    }
+
+    .article-bottom {
+      position: fixed;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      box-sizing: border-box;
+      height: 88px;
+      border-top: 1px solid #d8d8d8;
+      background-color: #fff;
+
+      .comment-btn {
+        width: 282px;
+        height: 46px;
+        border: 2px solid #eeeeee;
+        font-size: 30px;
+        line-height: 46px;
+        color: #a7a7a7;
+      }
+
+      .van-icon {
+        font-size: 40px;
+
+        .van-info {
+          font-size: 16px;
+          background-color: #e22829;
+        }
+      }
+    }
+
+    .article-bottom {
+      position: fixed;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      box-sizing: border-box;
+      height: 88px;
+      border-top: 1px solid #d8d8d8;
+      background-color: #fff;
+
+      .comment-btn {
+        width: 282px;
+        height: 46px;
+        border: 2px solid #eeeeee;
+        font-size: 30px;
+        line-height: 46px;
+        color: #a7a7a7;
+      }
+
+      /deep/ .van-icon {
+        font-size: 40px;
+      }
+
+      .comment-icon {
+        top: 2px;
+        color: #777;
+
+        .van-info {
+          font-size: 16px;
+          background-color: #e22829;
+        }
+      }
+
+      .btn-item {
+        border: none;
+        padding: 0;
+        height: 40px;
+        line-height: 40px;
+        color: #777777
+      }
+
+      .collect-btn--collected {
+        color: #ffa500;
+      }
+
+      .like-btn--liked {
+        color: #e5645f;
       }
     }
   }
-
-  .loading-wrap {
-    padding: 200px 32px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-  }
-
-  .error-wrap {
-    padding: 200px 32px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-    .van-icon {
-      font-size: 122px;
-      color: #b4b4b4;
-    }
-    .text {
-      font-size: 30px;
-      color: #666666;
-      margin: 33px 0 46px;
-    }
-    .retry-btn {
-      width: 280px;
-      height: 70px;
-      line-height: 70px;
-      border: 1px solid #c3c3c3;
-      font-size: 30px;
-      color: #666666;
-    }
-  }
-
-  .article-bottom {
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    box-sizing: border-box;
-    height: 88px;
-    border-top: 1px solid #d8d8d8;
-    background-color: #fff;
-    .comment-btn {
-      width: 282px;
-      height: 46px;
-      border: 2px solid #eeeeee;
-      font-size: 30px;
-      line-height: 46px;
-      color: #a7a7a7;
-    }
-    .van-icon {
-      font-size: 40px;
-      .van-info {
-        font-size: 16px;
-        background-color: #e22829;
-      }
-    }
-  }
-  .article-bottom {
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    box-sizing: border-box;
-    height: 88px;
-    border-top: 1px solid #d8d8d8;
-    background-color: #fff;
-    .comment-btn {
-      width: 282px;
-      height: 46px;
-      border: 2px solid #eeeeee;
-      font-size: 30px;
-      line-height: 46px;
-      color: #a7a7a7;
-    }
-    /deep/ .van-icon {
-      font-size: 40px;
-    }
-    .comment-icon {
-      top: 2px;
-      color: #777;
-      .van-info {
-        font-size: 16px;
-        background-color: #e22829;
-      }
-    }
-    .btn-item {
-      border: none;
-      padding: 0;
-      height: 40px;
-      line-height: 40px;
-      color: #777777
-    }
-    .collect-btn--collected {
-      color: #ffa500;
-    }
-    .like-btn--liked {
-      color: #e5645f;
-    }
-  }
-}
 </style>
